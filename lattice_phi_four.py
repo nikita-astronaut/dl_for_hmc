dc49d40807048b0cbc21928bc187e3c93040f906
nikita-astronaut <nikita.astronaut@gmail.com>
2018-08-29 12:54:53 +0300

    added force computation

--- lattice_phi_four.py
+++ lattice_phi_four.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def get_action(configuration, kappa, lamb):
 	'''
+	calculates action, see Montvay&Munster formula (2.9)
 	configuration: D-dimentional python array with the configuration
 	kappa: action parameter
 	lamb: action parameter lambda
@@ -15,3 +16,18 @@ def get_action(configuration, kappa, lamb):
 	action += np.sum(configuration ** 2)
 	action += lamb * np.sum((configuration ** 2 - 1.0) ** 2 - 1.0)
 	return action
+
+def get_force(configuration, kappa, lamb):
+	'''
+	calculates force of the action $f_x = \partial S / \partial \phi_x$
+	configuration: D-dimentional python array with the configuration
+	kappa: action parameter
+	lamb: action parameter lambda
+	'''
+	force = 2 * configuration
+	ndims = len(configuration.shape)
+
+	for dim in range(ndims):
+		force += -2 * kappa * (np.roll(configuration, shift = 1, axis = dim) + np.roll(configuration, shift = -1, axis = dim))
+	force += 4 * lamb * (configuration ** 3 - configuration)
+	return force
